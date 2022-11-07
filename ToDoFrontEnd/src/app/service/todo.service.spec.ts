e89172a482247119b45244ed7363f7f272459828
Bo Zhao <BZhao7@slb.com>
2022-11-07 16:51:52 +0800

    test(wenrui): service create function test
    Co-authored-by: wenruiLi <wenruiLiA15@users.noreply.github.com>

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ToDoItem } from '../model/ToDoItem';
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
 
@@ -6,8 +7,9 @@ describe('TodoService', () => {
 
   let service: TodoService;
   let todoStoreService: TodoStoreService;
-
+  let httpClient: any;
   beforeEach(() => {
+    httpClient = jasmine.createSpyObj('HttpClient', ['post']);
     todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
@@ -15,5 +17,16 @@ describe('TodoService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create todoItem when using mocked httpPost request', () => {
+    // given
+    const todoItem = new ToDoItem(0, '', '', false);
+    // when
+    service.create(todoItem);
+    // then
+    expect(httpClient.post).toHaveBeenCalledWith(
+      'https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos',todoItem
+    )
   });
 });
